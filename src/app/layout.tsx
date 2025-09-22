0553b13b7361d5f3dc2f1782f00e110a612102b4
parintnk <parin.tnk@gmail.com>
2025-09-22 16:06:49 +0700

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
+      <body className="noto-sans-thai">
         {children}
       </body>
     </html>
